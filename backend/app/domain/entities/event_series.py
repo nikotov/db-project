1bf205acf23b3f59d3c7fefca7735627b185e68c
¨nikotov¨ <nicolastovar18@hotmail.com>
2026-04-23 11:13:21 -0500

    domain

--- backend/app/domain/entities/event_series.py
+++ backend/app/domain/entities/event_series.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from datetime import date
-from app.domain.enums import AttendanceType
+from app.domain.enums.attendance_type import AttendanceType
 
 @dataclass
 class EventSeries:
